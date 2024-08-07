0628b23af5eb44c6deb449c695860a5f64acff5b
madhankumar028 <madhankumar028@gmail.com>
2024-08-07 21:36:10 +0530

    adds middleware to pass the url path and searchparams in headers

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Add a new header x-current-path which passes the path to downstream components
+  const headers = new Headers(request.headers);
+  
+  const url = new URL(request.url);
+  const queryParams = url.searchParams;
+  const category = queryParams.get('category');
+
+  headers.set("x-current-path", request.nextUrl.pathname);
+  headers.set("x-current-category", category);
+
+  return NextResponse.next({ headers });
+};
+
+export const config = {
+  matcher: [
+    // match all routes except static files and APIs
+    "/((?!api|_next/static|_next/image|favicon.ico|favicon-dark.svg|favicon.svg).*)",
+  ],
+};
