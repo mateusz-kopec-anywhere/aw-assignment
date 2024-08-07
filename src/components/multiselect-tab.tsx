788e6c47834e901c4038824be762210bf47be42a
madhankumar028 <madhankumar028@gmail.com>
2024-08-07 21:52:18 +0530

    adds title and hides the nmulti select tab in mobile view

--- src/components/multiselect-tab.tsx
+++ src/components/multiselect-tab.tsx
@@ -31,9 +31,14 @@ export const MultiSelectTab = ({ tabItems }: IMultiSelectTabProps) => {
   };
 
   return (
-    <div className='gap-4 pb-20 pt-10 text-base lg:text-sm'>
+    <div className='gap-4 pb-20 pt-10 text-base lg:text-sm hidden lg:relative lg:flex'>
+      <h2
+        className="text-lg text-gray-900 font-strong dark:text-white"
+      >
+        Categories
+      </h2>
       <ul
-        className='flex flex-col gap-2 list-none'
+        className='flex flex-col gap-2 list-none mt-2'
         role='list'
       >
         {
